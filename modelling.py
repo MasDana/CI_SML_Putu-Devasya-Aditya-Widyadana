02fe99ac1bc3708d9e2e5c83910f34e437f9a33b
MasDana <putudeni1414@gmail.com>
2025-12-23 02:09:11 +0800

    menambahkan library

---
+++ modelling.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+import joblib
+import os
