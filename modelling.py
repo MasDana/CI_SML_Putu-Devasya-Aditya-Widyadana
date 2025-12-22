95a3f76045d8caf5d69010196443d21de3153b7f
MasDana <putudeni1414@gmail.com>
2025-12-23 02:13:31 +0800

    set mlflow, dataset, train test split

--- modelling.py
+++ modelling.py
@@ -7,3 +7,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
 import joblib
 import os
+
+mlflow.set_experiment("CI Wine Quality Training")
+
+df = pd.read_csv("C:/Users/Microsoft/Documents/CI_SML_PUTU DEVASYA ADITYA WIDYADANA/winequality-white_preprocessing.csv")
+
+X = df.drop("quality", axis=1)
+y = df["quality"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
